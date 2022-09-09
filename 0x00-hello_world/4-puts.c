8d11962a3619bb8e5aee83a2fa79b55c0cb87a96
Mannie <ogunwoleemmanuel123@gmail.com>
2022-09-09 15:55:09 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- Serves as the entry point for the program
+ * 
+ *Return: By default returns 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
